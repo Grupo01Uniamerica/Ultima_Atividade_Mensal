d322f8b06f1a25fbda0f6b0f2d51d0f983a683d3
Scheligan Gabriele Monzon <97247232+Scheeee@users.noreply.github.com>
2022-12-15 00:54:03 -0300

    favoritos 89% pronto

---
+++ teste/index.js
@@ -0,0 +1,222 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '48eff17a7bmsh229331f0d4915dfp1dc46ejsnc8c299b51932',
+		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+	}
+};
+
+let cards = document.getElementById('cards');
+let counter = 0;
+
+let JOGOS;
+let array = [];
+let numFav;
+var arrayFavoritos = localStorage.getItem("favoritos");
+if( arrayFavoritos != null){
+ array = JSON.parse(arrayFavoritos);
+ numFav= localStorage.getItem("numFav");
+}
+
+
+let choicedUrl = 'sort-by=popularity';
+let choicedPlataform = 'platform=all&';
+let choicedTeste = 'platform=all&sort-by=popularity'
+lerApi(choicedTeste);
+
+
+let pcButton = document.getElementById('pc-button');
+pcButton.addEventListener('click',function(){choicedPlat("platform=pc&")})
+
+let browserButton = document.getElementById('browser-button');
+browserButton.addEventListener('click',function(){choicedPlat("platform=browser&")})
+
+let allButton = document.getElementById('all-button');
+allButton.addEventListener('click',function(){choicedPlat("platform=all&")})
+
+let favbutton = document.getElementById('fav-buttonn');
+allButton.addEventListener('click',favoritos());
+
+
+
+let homeButton = document.getElementById('home-button');
+homeButton.addEventListener('click', function(){choicedGenre("sort-by=popularity")});
+
+let sportsButton = document.getElementById('sports-button');
+sportsButton.addEventListener('click', function(){choicedGenre("category=sports")});
+
+let mmoButton = document.getElementById('mmo-button');
+mmoButton.addEventListener('click', function(){choicedGenre("category=mmorpg")});
+
+let mobaButton = document.getElementById('moba-button');
+mobaButton.addEventListener('click', function(){choicedGenre("category=moba")});
+
+let cardButton = document.getElementById('card-button');
+cardButton.addEventListener('click', function(){choicedGenre("category=card")});
+
+let lutaButton = document.getElementById('fighting-button');
+lutaButton.addEventListener('click', function(){choicedGenre("category=fighting")});
+
+let shootingButton = document.getElementById('shooting-button');
+shootingButton.addEventListener('click', function(){choicedGenre("category=shooter")});
+
+let racingButton = document.getElementById('racing-button');
+racingButton.addEventListener('click', function(){choicedGenre("category=racing")});
+
+let maisButton = document.getElementById('mais');
+maisButton.addEventListener('click',function(){lerApi2(choicedTeste)})
+
+
+
+function choicedPlat(selectedPlat){
+
+    counter = 0;
+    choicedPlataform = selectedPlat;
+    choicedTeste = choicedPlataform + choicedUrl;
+    lerApi(choicedTeste);
+
+}
+
+function choicedGenre(selectedGenre) {
+    counter = 0;
+    choicedUrl = selectedGenre;
+    choicedTeste = choicedPlataform + choicedUrl;
+    lerApi(choicedTeste);
+}
+
+function lerApi(choicedTeste){
+    cards.innerHTML = '';
+fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?${choicedTeste}`, options)
+	.then(response => response.json())
+
+	.then(data => {
+        banner(data);
+        JOGOS = data;
+    })
+	.catch(err => console.log(err));
+
+}
+
+function lerApi2(choicedTeste){
+fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?${choicedTeste}`, options)
+	.then(response => response.json())
+	.then(data => banner(data))
+	.catch(err => console.log(err));
+
+}
+function favoritar(i){
+    
+    data = JOGOS;
+
+    for(let a = 0; a < array.length; a++){
+
+        if(data[i].id== array[a].id){
+
+            return;
+        }
+
+
+
+    }
+   
+
+    alert('bomdia' + i);
+
+    
+    let jogos = adicionar(i, data);
+    adiciona_jogos(jogos);
+
+   
+    
+    favoritos();
+
+
+}
+
+function adiciona_jogos(jogos){
+   
+    array.push(jogos);
+    numFav++;
+
+
+    
+
+    localStorage.setItem("favoritos",JSON.stringify(array));
+ 
+    alert("morri");
+}
+function adicionar(i, data){
+     let jogos = {}
+    
+    jogos.id = data[i].id; 
+    jogos.nome = data[i].title;
+    jogos.url = data[i].freetogame_profile_url;
+    jogos.img = data[i].thumbnail; 
+   
+    
+   
+    return jogos;
+}
+
+
+function favoritos(){
+
+    console.log(array);
+
+       for(let i = 0; i < array.length; i++){
+
+        let article = document.createElement('article');
+        article.className = "tese"
+        article.innerHTML = `  
+        
+        
+        <a  href=${array[i].url} target="_blank">
+        <img id="aa${i}"src="${array[i].img}">
+        <div id="flex">
+        <h1 id="nome${i}">${array[i].nome}</h1> 
+        </a>
+        <a class="estrela" id= ${i} onclick="favoritar(${i})">☆</a>
+        </a>
+        </div>
+        `
+
+
+        cards.appendChild(article);
+
+       }
+
+
+
+}
+
+
+function banner(data){
+ 
+    for ( i = counter; i < counter + 9; i++){
+        let article = document.createElement('article');
+        article.className = "tes"
+        article.innerHTML = `  
+        
+        
+        <a  href=${data[i].freetogame_profile_url} target="_blank">
+        <img id="img${i}"src="${data[i].thumbnail}">
+        <div id="flex">
+        <h1 id="txt${i}">${data[i].title}</h1> 
+        </a>
+        <a class="estrela" id= ${i} onclick="favoritar(${i})">☆</a>
+        </a>
+        </div>
+        `
+
+
+        cards.appendChild(article);
+    }
+    counter+=10;
+}
+
+
+
+
+
+
+
